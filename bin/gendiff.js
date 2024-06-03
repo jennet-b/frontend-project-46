1761a3bbb1f5ae42a71aa04cf805b35aa0c45fb6
Dzhennet Berdieva <berdievajennet@yandex.ru>
2024-06-03 20:38:18 +0300

    step 2 Arguments and options

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -5,5 +5,8 @@ import { program } from 'commander';
 program
   .description('Compares two configuration files and shows a difference.')
   .version('1.0.0')
+  .argument('<filepath1>')
+  .argument('<filepath2>')
+  .option('-f, --format [type]', 'output format')
 
   program.parse();
